08a92151fb4d0053fa4b5b53807fc80e9e538a81
Rakshitha VM <gowdarakshithavm@gmail.com>
2026-04-05 16:20:29 +0530

    fix dashboard imports

--- frontend/src/Dashboard.js
+++ frontend/src/Dashboard.js
@@ -1,5 +1,4 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import React from "react";
 
 import AddEvent from "./components/AddEvent";
 import EventList from "./components/EventList";
